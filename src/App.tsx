9a020650177141923e4aaeba6b825d7f8b6bea86
Raghav <raghavchauhan179@gmail.com>
2026-05-28 20:40:46 +0530

    update ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Hero       from './components/Hero/Hero'
 import About      from './components/About/About'
 import Projects   from './components/Projects/Projects'
 import Skills     from './components/Skills/Skills'
-import Experience from './components/Experience/Experience'
 import Contact    from './components/Contact/Contact'
 import Footer     from './components/Footer/Footer'
 import { BeamsBackground } from './components/ui/beams-background'
@@ -20,7 +19,6 @@ export default function App() {
         <About />
         <Projects />
         <Skills />
-        <Experience />
         <Contact />
         <Footer />
       </div>
